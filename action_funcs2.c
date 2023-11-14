6bf38043b7b0342dd86da1e083326cbad25d17c4
Ezekiel Bassey <Bazzdude007@gmail.com>
2023-11-15 00:23:39 +0100

    %b idnetifier added

---
+++ action_funcs2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * int_binar - writes or converts unsignedinteger
+ * to binary number
+ * @va_list: number of unsigned int
+ * Return: The counter for the written byte 
+*/
+int int_binary (va_list args) 
+{
+	unsigned int n;
+	int count;
+
+	count = 0;
+	n = va_arg(args, unsigned int);
+	count += _print_digit((long)n, 2);
+
+	return (count);
+}
