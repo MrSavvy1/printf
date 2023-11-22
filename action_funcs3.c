39772daf6859cacd402ec73d9a440d27dd358ec6
MrSavvy1 <destydesty112@gmail.com>
2023-11-22 14:12:53 -0800

    logs

--- action_funcs3.c
+++ action_funcs3.c
@@ -17,14 +17,9 @@ int str_rev(va_list args)
 	{
 		return (write(1, "(null)", 6));
 	}
-	while (*s && s[i] != '\0')
+	while (*s)
 	{
-		char strin = s[i];
-		i += 1;
-	}
-	for (int j = i - 1; j >= 0; j--)
-	{
-		count += write(1, &s[j], 1);
+		count += write(1, --s, 1);
 	}
 	return (count);
 }
